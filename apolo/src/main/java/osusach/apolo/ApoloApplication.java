04a166b4595ebebdf66f1024d867fd332fa88787
Andres Zelaya <andres.zelaya.d@hotmail.com>
2024-06-07 17:24:57 -0400

    Spring upload
    Spring proyect upload

---
+++ apolo/src/main/java/osusach/apolo/ApoloApplication.java
@@ -0,0 +1,13 @@
+package osusach.apolo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApoloApplication.class, args);
+	}
+
+}
---
+++ apolo/src/test/java/osusach/apolo/ApoloApplicationTests.java
@@ -0,0 +1,13 @@
+package osusach.apolo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApoloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
